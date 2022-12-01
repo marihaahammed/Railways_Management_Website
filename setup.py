6cf6bf46f4611dde804c45d37ba56108023aeda8
Yaseen <yshaikh@kent.edu>
2022-12-01 11:43:39 -0500

    setup.py completed but still need to fill all tables except user

--- setup.py
+++ setup.py
@@ -52,3 +52,108 @@ cursor.execute("CREATE DATABASE myDB")
   			PRIMARY KEY (track_ID)
 		)
 	''')
+
+	cursor.execute('''
+		CREATE TABLE Schedule
+		(
+  			sched_ID INT NOT NULL,
+  			source INT NOT NULL,
+  			destination INT NOT NULL,
+  			start_time INT NOT NULL,
+  			end_time INT NOT NULL,
+  			PRIMARY KEY (sched_ID)
+		)
+	''')
+
+	cursor.execute('''
+		CREATE TABLE User
+		(
+  			username VARCHAR(20) NOT NULL,
+  			password VARCHAR(20) NOT NULL,
+  			first_name VARCHAR(15) NOT NULL,
+  			last_name VARCHAR(15) NOT NULL,
+  			user_ID INT NOT NULL,
+  			role_ID INT NOT NULL,
+  			PRIMARY KEY (user_ID)
+		)
+	''')
+
+	cursor.execute('''
+		CREATE TABLE track_route
+		(
+  			route_ID INT NOT NULL,
+  			track_ID INT NOT NULL,
+  			PRIMARY KEY (route_ID, track_ID),
+  			FOREIGN KEY (route_ID) REFERENCES Route(route_ID),
+  			FOREIGN KEY (track_ID) REFERENCES Track(track_ID)
+		)
+	''')
+
+	cursor.execute('''
+		CREATE TABLE sched_station
+		(
+  			station_ID INT NOT NULL,
+  			sched_ID INT NOT NULL,
+  			PRIMARY KEY (station_ID, sched_ID),
+  			FOREIGN KEY (station_ID) REFERENCES Station(station_ID),
+  			FOREIGN KEY (sched_ID) REFERENCES Schedule(sched_ID)
+		)
+	''')
+
+	cursor.execute('''
+		CREATE TABLE route_station
+		(
+  			route_ID INT NOT NULL,
+  			station_ID INT NOT NULL,
+  			PRIMARY KEY (route_ID, station_ID),
+  			FOREIGN KEY (route_ID) REFERENCES Route(route_ID),
+  			FOREIGN KEY (station_ID) REFERENCES Station(station_ID)
+		)
+	''')
+
+	cursor.execute('''
+		CREATE TABLE Train
+		(
+  			train_ID INT NOT NULL,
+  			train_length INT NOT NULL,
+  			route_ID INT NOT NULL,
+  			sched_ID INT NOT NULL,
+  			PRIMARY KEY (train_ID),
+  			FOREIGN KEY (route_ID) REFERENCES Route(route_ID),
+  			FOREIGN KEY (sched_ID) REFERENCES Schedule(sched_ID)
+		)
+	''')
+
+	cursor.execute('''
+		CREATE TABLE Cargo
+		(
+  			cargo_ID INT NOT NULL,
+  			type INT NOT NULL,
+  			weight INT NOT NULL,
+  			owner VARCHAR(10) NOT NULL,
+  			car_number INT NOT NULL,
+  			train_ID INT NOT NULL,
+  			PRIMARY KEY (cargo_ID),
+  			FOREIGN KEY (train_ID) REFERENCES Train(train_ID)
+		)
+	''')
+
+	cursor.execute('''
+		CREATE TABLE train_track
+		(
+  			train_ID INT NOT NULL,
+  			track_ID INT NOT NULL,
+  			PRIMARY KEY (train_ID, track_ID),
+  			FOREIGN KEY (train_ID) REFERENCES Train(train_ID),
+  			FOREIGN KEY (track_ID) REFERENCES Track(track_ID)
+		)
+	''')
+
+	cursor.execute('''INSERT INTO User (username, password, first_name, last_name, user_ID, role_ID) VALUES ('yshaikh', 'y_pword', 'yaseen', 'shaikh', 0, 0)''')
+	cursor.execute('''INSERT INTO User (username, password, first_name, last_name, user_ID, role_ID) VALUES ('kpatel', 'k_pass', 'khushi', 'patel', 1, 1''')
+	cursor.execute('''INSERT INTO User (username, password, first_name, last_name, user_ID, role_ID) VALUES ('bgall', 'b_pass', 'brandon', 'gall', 2, 0)''')
+	cursor.execute('''INSERT INTO User (username, password, first_name, last_name, user_ID, role_ID) VALUES ('mahammed', 'm_pword', 'mariha', 'ahammed', 3, 1)''')
+	cursor.execute('''INSERT INTO User (username, password, first_name, last_name, user_ID, role_ID) VALUES ('jjoseph', 'j_pword', 'jackson', 'joseph', 4, 1)''')
+
+
+	connection.close()
