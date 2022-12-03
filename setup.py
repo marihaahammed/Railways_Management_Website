2e73f0c3a75cfd2e952befe70ad94bc92535257f
Yaseen Shaikh <92325322+tisFlamio@users.noreply.github.com>
2022-12-03 10:45:42 -0500

    Update setup.py
    just commented out the line where i create a new database.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ password = "dJTy7cg2",
 
 cursor = db.cursor()
 
-cursor.execute("CREATE DATABASE myDB")
+#cursor.execute("CREATE DATABASE myDB")
 
 #with initial setup out of the way, we can now create the contents of the database
 #this .py file was created as a preferred way of creating and filling the database instead of a .php file
