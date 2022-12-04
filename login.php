91cdacfd69684ee2889242e733343a55603e76de
bgall1 <bgall1@kent.edu>
2022-12-04 16:27:16 -0500

    update

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect them to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: railways.html");
+    header("location:/templates/railways.html");
     exit;
 }
  
