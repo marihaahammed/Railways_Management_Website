87c27abfcceaeb0fce489bad0b40f989fe7f41ce
bgall1 <bgall1@kent.edu>
2022-12-04 16:31:45 -0500

    update

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'yshaikh'); //Yaseen will fill in
 define('DB_PASSWORD', 'dJTy7cg2'); //Yasee will fil in
-define('DB_NAME', 'user');
+define('DB_NAME', 'yshaikh');
  
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
