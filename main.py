17ed5dd545182e49058bf02bc8eb1d7bf4ee6668
bgall1 <bgall1@kent.edu>
2022-12-05 13:09:26 -0500

    Overhall of main.py and our login system

--- main.py
+++ main.py
@@ -17,13 +17,14 @@ app.config['MYSQL_DB'] = 'yshaikh'
 # Intialize MySQL
 mysql = MySQL(app)
 
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    msg = ''
-    return render_template('railways.html', msg = msg)
+# @app.route('/home', methods=['GET', 'POST'])
+# def home():
+#     msg = ''
+#     return render_template('railways.html', msg = msg)
 
 # http://localhost:5000/pythonlogin/ - the following will be our login page, which will use both GET and POST requests
-@app.route('/pythonlogin/', methods=['GET', 'POST'])
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     # Output message if something goes wrong...
     msg = ''
@@ -53,7 +54,7 @@ def login():
 
     # http://localhost:5000/python/logout - this will be the logout page
 
-@app.route('/pythonlogin/logout')
+@app.route('/logout')
 def logout():
     # Remove session data, this will log the user out
    session.pop('loggedin', None)
@@ -61,6 +62,48 @@ def logout():
    session.pop('username', None)
    # Redirect to login page
    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM Users WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO Users VALUES (NULL, %s, %s)', (username, password,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
+@app.route('/home')
+def home():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('railways.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+
 
 #localhost:5003/cargo
 @app.route('/cargo', methods = ['GET', 'POST'])
@@ -130,39 +173,7 @@ def sched_search():
     schedules = cursor.fetchall()
     return render_template('schedSearch.html', schedules=schedules)
 # http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
-#@app.route('/pythonlogin/register', methods=['GET', 'POST'])
-#def register():
-    # Output message if something goes wrong...
- #   msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
-  #  if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        # Create variables for easy access
-   #     username = request.form['username']
-    #    password = request.form['password']
-     #   email = request.form['email']
-        # Check if account exists using MySQL
-      #  cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-       # cursor.execute('SELECT * FROM User WHERE username = %s', (username,))
-        #account = cursor.fetchone()
-        # If account exists show error and validation checks
-        #if account:
-        #    msg = 'Account already exists!'
-        #elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-        #    msg = 'Invalid email address!'
-        #elif not re.match(r'[A-Za-z0-9]+', username):
-        #    msg = 'Username must contain only characters and numbers!'
-        #elif not username or not password or not email:
-        #    msg = 'Please fill out the form!'
-        #else:
-            # Account doesnt exists and the form data is valid, now insert new account into accounts table
-         #   cursor.execute('INSERT INTO User VALUES (NULL, %s, %s, %s)', (username, password, email,))
-          #  mysql.connection.commit()
-           # msg = 'You have successfully registered!'
-    #elif request.method == 'POST':
-        # Form is empty... (no POST data)
-     #   msg = 'Please fill out the form!'
-    # Show registration form with message (if any)
-    #return render_template('register.html', msg=msg)
+
 
 @app.route('/search', methods = ['POST', 'GET'])
 def search():
