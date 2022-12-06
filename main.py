ac7477e7ae11ab0b880d9591e8549d5db8cc6267
Shaikh <yshaikh@dbdev.cs.kent.edu>
2022-12-06 12:00:21 -0500

    main

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def about():
 # http://localhost:5000/pythonlogin/ - the following will be our login page, which will use both GET and POST requests
 
 @app.route('/EmployeePortal')
-def about():
+def emp():
      msg = ''
      return render_template('employee.html', msg=msg)
      
