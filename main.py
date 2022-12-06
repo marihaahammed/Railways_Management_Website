8f758b501215e8d2f0a43d0bea6e4998bd8625b4
bgall1 <bgall1@kent.edu>
2022-12-05 19:12:32 -0500

    hopefully works

--- main.py
+++ main.py
@@ -72,10 +72,12 @@ def register():
     # Output message if something goes wrong...
     msg = ''
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'first_name' in request.form and 'last_name' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
+        firstName = request.form['first_name']
+        lastName = request.form['last_name']
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM Users WHERE username = %s', (username,))
@@ -89,7 +91,7 @@ def register():
             msg = 'Please fill out the form!'
         else:
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO Users VALUES (%s, %s)', (username, password,))
+            cursor.execute('INSERT INTO Users VALUES (%s, %s, %s, %s, Null, 1)', (username, password, firstName, lastName))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
